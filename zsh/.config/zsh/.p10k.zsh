99992d69f1eb3331c234f6735d7c9ece994b1177
Fabian Preuß <preuss_fabian@gmx.de>
2022-03-14 22:31:43 +0100

    Transient prompt for zsh
    Only display postcmd stats when longer than 10s or $? no zero

--- zsh/.config/zsh/.p10k.zsh
+++ zsh/.config/zsh/.p10k.zsh
@@ -32,9 +32,6 @@
   # The list of segments shown on the left. Fill it with the most important segments.
   typeset -g POWERLEVEL9K_LEFT_PROMPT_ELEMENTS=(
     # =========================[ Line #1 ]=========================
-    # status                  # exit code of the last command
-    # command_execution_time  # duration of the last command
-    # os_icon                 # os identifier
     context                 # user@hostname
     anaconda
     dir                     # current directory
@@ -545,7 +542,7 @@
   #   - always:   Trim down prompt when accepting a command line.
   #   - same-dir: Trim down prompt when accepting a command line unless this is the first command
   #               typed after changing current working directory.
-  typeset -g POWERLEVEL9K_TRANSIENT_PROMPT=off
+  typeset -g POWERLEVEL9K_TRANSIENT_PROMPT=same-dir
 
   # Instant prompt mode.
   #
--- zsh/.config/zsh/zsh_command_info.zsh
+++ zsh/.config/zsh/zsh_command_info.zsh
@@ -2,47 +2,52 @@
 
 autoload -U colors && colors
 
-_cust_time_preexec() {
-	ZSH_COMMAND_TIME_START=${ZSH_COMMAND_TIME_START:-$(date "+%s%1N")}
+_zsh_time_preexec() {
+	ZSH_COMMAND_TIME_START=$(date "+%s%1N")
 }
 
-_cust_time_precmd() {
-	if [ $ZSH_COMMAND_TIME_START ]; then
-		local execution_time_mil=$(( $(date "+%s%1N") - ${ZSH_COMMAND_TIME_START} ))
-		local execution_time_sec=$(( ${execution_time_mil} / 10 ))
+_zsh_time_precmd() {
+	[ $ZSH_COMMAND_TIME_START ] || return
 
-		local hrs=$(( ${execution_time_sec} / 3600 ))
-		local min=$(( ${execution_time_sec} / 60 % 60 ))
-		local sec=$(( ${execution_time_sec} % 60 ))
-		local mil=$(( ${execution_time_mil} % 10 ))
+	local execution_time_mil=$(( $(date "+%s%1N") - ${ZSH_COMMAND_TIME_START} ))
+	local execution_time_sec=$(( ${execution_time_mil} / 10 ))
+	unset ZSH_COMMAND_TIME_START
 
-		local timer_display=""
-		if [ ${hrs} -gt 0 ]; then
-			timer_display=$(printf "%dh %dm %ds" "$hrs" "$min" "$sec")
-		elif [ ${min} -gt 0 ]; then
-			timer_display=$(printf "%dm %ds" "$min" "$sec")
-		else
-			timer_display=$(printf "%d.%ds" "$sec" "$mil")
-		fi
-
-		COMMAND_TIME_INFO="%101F ${timer_display}%f"
-		unset ZSH_COMMAND_TIME_START
+	if [ ${execution_time_sec} -lt 10 ] && [ "${EXIT_ERROR:-0}" -eq 0 ]; then
+		return
 	fi
+
+	local mil=$(( ${execution_time_mil} % 10 ))
+	local sec=$(( ${execution_time_sec} % 60 ))
+	local min=$(( ${execution_time_sec} / 60 % 60 ))
+	local hrs=$(( ${execution_time_sec} / 3600 ))
+
+	local timer_display=""
+	if [ ${hrs} -gt 0 ]; then
+		timer_display=$(printf "%dh %dm %ds" "$hrs" "$min" "$sec")
+	elif [ ${min} -gt 0 ]; then
+		timer_display=$(printf "%dm %ds" "$min" "$sec")
+	else
+		timer_display=$(printf "%d.%ds" "$sec" "$mil")
+	fi
+
+	COMMAND_TIME_INFO="%101F ${timer_display}%f"
 }
 
-_cust_status_precmd() {
+_zsh_status_precmd() {
 	# save exit codes in variable because "local" is a command and overwrites $? and $pipestatus
 	local exit_codes=("${pipestatus[@]}")
 
 	local status_info=""
 	local status_color=""
-	local ec_not_zero=0
+
+	EXIT_ERROR=0
 
 	local stat
 	for stat in ${exit_codes}; do
 		if [ ${stat} -gt 0 ]; then
 			# Remember error for later
-			ec_not_zero=1
+			EXIT_ERROR=1
 		fi
 
 		if [ ${stat} -gt 128 ];then
@@ -60,7 +65,7 @@ _cust_status_precmd() {
 		status_color="❌%160F"
 	fi
 
-	if [ ${ec_not_zero} -eq 1 ]; then
+	if [ ${EXIT_ERROR} -ne 0 ]; then
 		# Remove leading "|" from $status_info
 		COMMAND_STATUS_INFO="${status_color} ${status_info#\|}%f"
 	else
@@ -68,18 +73,17 @@ _cust_status_precmd() {
 	fi
 }
 
-_cust_print_cmd_info() {
-	if [ $COMMAND_TIME_INFO ]; then
+_zsh_print_cmd_info() {
+	if [ "${COMMAND_TIME_INFO}" ] || [ "${EXIT_ERROR}" -ne 0 ]; then
 		print -P "─╸\[${COMMAND_STATUS_INFO}\]\-\[${COMMAND_TIME_INFO}\]\-\[ %T\]"
 	fi
-
 	unset COMMAND_TIME_INFO
 }
 
-preexec_functions+=(_cust_time_preexec)
+preexec_functions+=(_zsh_time_preexec)
 
 # status check MUST be the first element to preserve $pipestatus (exit codes) of cmdline
-precmd_functions=(_cust_status_precmd "${precmd_functions[@]}")
-precmd_functions+=(_cust_time_precmd)
-precmd_functions+=(_cust_print_cmd_info)
+precmd_functions=(_zsh_status_precmd "${precmd_functions[@]}")
+precmd_functions+=(_zsh_time_precmd)
+precmd_functions+=(_zsh_print_cmd_info)
 
